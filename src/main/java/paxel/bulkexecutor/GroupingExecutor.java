69bb510acae9fd4e9f9a6128e2306e2f94fb924b
paxel <axel.evil@gmail.com>
2020-07-21 23:17:19 +0200

    added factory for special errorhandlers

--- src/main/java/paxel/bulkexecutor/GroupingExecutor.java
+++ src/main/java/paxel/bulkexecutor/GroupingExecutor.java
@@ -1,7 +1,9 @@
 package paxel.bulkexecutor;
 
 import paxel.bulkexecutor.internal.SingleSourceSequentialProcessor;
+
 import java.util.concurrent.ExecutorService;
+
 import paxel.bulkexecutor.internal.MultiSourceSequentialProcessor;
 
 /**
@@ -12,7 +14,6 @@ import paxel.bulkexecutor.internal.MultiSourceSequentialProcessor;
  * actor has one Processor. The actor lets the Processor execute the actors
  * actions and it is guaranteed, that no two processes of this actor run
  * parallel.
- *
  */
 public class GroupingExecutor {
 
@@ -71,5 +72,51 @@ public class GroupingExecutor {
      */
     public SequentialProcessor createMultiSourceBoundedSequentialProcessor(int limit) {
         return new MultiSourceSequentialProcessor(executorService, DEFAULT, limit);
+    }
+
+
+    /**
+     * This creates an unbounded SequentialProcessor, for singleThreaded Input.
+     *
+     * @return the SequentialProcessor.
+     */
+    public SequentialProcessor createSingleSourceSequentialProcessor(ErrorHandler errorHandler) {
+        return new SingleSourceSequentialProcessor(executorService, errorHandler);
+    }
+
+    /**
+     * This creates an bounded SequentialProcessor, for singleThreaded Input. In
+     * case the number of unprocessed Runnables reaches the given limit, no more
+     * Runnables are excepted.
+     *
+     * @param limit The limit for unprocessed runnables.
+     * @return the SequentialProcessor.
+     */
+    public SequentialProcessor createSingleSourceBoundedSequentialProcessor(int limit, ErrorHandler errorHandler) {
+        return new SingleSourceSequentialProcessor(executorService, errorHandler, limit);
+    }
+
+    /**
+     * This creates an unbounded SequentialProcessor, for multithreaded Input.
+     * The input performance gets worse, the more threads try to add Runnables.
+     *
+     * @return the SequentialProcessor.
+     */
+    public SequentialProcessor createMultiSourceSequentialProcessor(ErrorHandler errorHandler) {
+        return new MultiSourceSequentialProcessor(executorService, errorHandler);
+    }
+
+    /**
+     * This creates an bounded SequentialProcessor, for multithreaded Input. In
+     * case the number of unprocessed Runnables reaches the given limit, no more
+     * Runnables are excepted. The input performance gets worse, the more
+     * threads try to add Runnables.
+     *
+     * @param limit        The limit for unprocessed runnables.
+     * @param errorHandler the handler for Exceptions that happen while processing a Runnable.
+     * @return the SequentialProcessor.
+     */
+    public SequentialProcessor createMultiSourceBoundedSequentialProcessor(int limit, ErrorHandler errorHandler) {
+        return new MultiSourceSequentialProcessor(executorService, errorHandler, limit);
     }
 }
